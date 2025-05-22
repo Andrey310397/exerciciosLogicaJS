0bd8d95e304115049c90fdf7d2cb52afe51942d8
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-21 22:41:40 -0300

    Exercício 4 - Inversor de valores

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-// Calculador de IMC:
+// Inversor de Valores:
 
-// Crie uma função chamada calcularIMC que recebe peso (em kg) e altura (em metros) como parâmetros.
-// A função deve calcular o Índice de Massa Corporal (IMC) usando a fórmula: $IMC = peso / (altura \* altura)$.
-// Retorne o valor do IMC.
-// Exemplo: calcularIMC(70, 1.75) deve retornar aproximadamente 22.85.
 
-calcularIMC(70, 1.75);
+// Crie uma função chamada inverterValores que recebe duas variáveis, a e b.
+// A função deve trocar os valores de a e b entre si.
+// Dica: Você precisará de uma terceira variável temporária.
+// Exemplo: Se a = 'laranja' e b = 'limao', após chamar inverterValores(a, b), a deve ser 'limao' e b deve ser 'laranja'. (Lembre-se que você precisará usar console.log dentro da função para mostrar a troca ou retornar um array [a, b]).
 
-function calcularIMC(peso, altura) {
-    let imc = peso / (altura * altura);
-
-    console.log(imc);
+function inverterValores(val1, val2) {
+    let temp = val1;
+    val1 = val2;
+    val2 = temp;
+    return [val1, val2];
 }
+
+let a = 'laranja';
+let b = 'limão';
+
+console.log('Antes da função:');
+console.log('A:', a);
+console.log('B:', b);
+
+let valoresTrocados = inverterValores(a, b);
+
+a = valoresTrocados[0];
+b = valoresTrocados[1];
+
+console.log('\nDepois da função:');
+console.log('A:', a);
+console.log('B:', b);
